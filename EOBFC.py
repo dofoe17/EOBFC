ef1c33ac0a087f80885b8047a39258199d2d49f9
dofoe17 <155560295+dofoe17@users.noreply.github.com>
2025-12-22 15:09:13 +0000

    Add files via upload

--- EOBFC.py
+++ EOBFC.py
@@ -9,7 +9,7 @@ st.title('EOBFC Fines 2025-26 ⚽💰')
 st.divider()
 
 #Define filepath
-filepath = pd.ExcelFile(r'C:\Users\Computer\OneDrive\Python\EOBFC\data\EOBFC fines list 25_26.xlsx')
+filepath = pd.ExcelFile(r'C:\Users\Computer\OneDrive\Python\EOBFC\EOBFC fines list 25_26.xlsx')
 filepath = pd.ExcelFile('data/EOBFC fines list 25_26.xlsx')
 
 sheet_names = ['GW1', 'GW2', 'GW3', 'GW4', 'GW5']
